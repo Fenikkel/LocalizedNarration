077abb6997df73aff1f8b8e3a4227360d4b00b70
Fenikkel <paufenollosa@gmail.com>
2024-04-25 12:03:11 +0200

    adding namespace

--- Runtime/LocalizedNarrationController.cs
+++ Runtime/LocalizedNarrationController.cs
@@ -13,165 +13,168 @@ using UnityEditor;
  * 
  * 
  */
-    public class LocalizedNarrationController : MonoBehaviour
+namespace LocalizedNarration
 {
-    public UnityEvent OnPreloadedNarration;
-
-    Coroutine _SpeechCoroutine;
-    Coroutine _PreloadCoroutine;
-
-    #region Singleton
-    private static LocalizedNarrationController _Instance = null;
-    public static LocalizedNarrationController Instance
+    public class LocalizedNarrationController : MonoBehaviour
     {
-        get
+        public UnityEvent OnPreloadedNarration;
+
+        Coroutine _SpeechCoroutine;
+        Coroutine _PreloadCoroutine;
+
+        #region Singleton
+        private static LocalizedNarrationController _Instance = null;
+        public static LocalizedNarrationController Instance
         {
-            return _Instance;
+            get
+            {
+                return _Instance;
+            }
         }
-    }
-    #endregion
+        #endregion
 
-    private void Awake()
-    {
-        CheckSingleton();
-    }
-
-    #region Controls
-    public static Coroutine Preload(Speech speech)
-    {
-        if (Instance == null)
+        private void Awake()
         {
-            Debug.LogWarning($"No {typeof(LocalizedNarrationController)} on the scene ");
-            return null;
+            CheckSingleton();
         }
 
-        return Instance.PreloadNarration(speech);
-    }
-
-    public Coroutine PreloadNarration(Speech speech)
-    {
-        if (LocalizedAudioController.Instance == null)
+        #region Controls
+        public static Coroutine Preload(Speech speech)
         {
-            Debug.LogWarning($"No {typeof(LocalizedAudioController)} on the scene ");
-            return null;
+            if (Instance == null)
+            {
+                Debug.LogWarning($"No {typeof(LocalizedNarrationController)} on the scene ");
+                return null;
+            }
+
+            return Instance.PreloadNarration(speech);
         }
 
-        if (LocalizedCaptionsController.Instance == null)
+        public Coroutine PreloadNarration(Speech speech)
         {
-            Debug.LogWarning($"No {typeof(LocalizedCaptionsController)} on the scene ");
-            return null;
+            if (LocalizedAudioController.Instance == null)
+            {
+                Debug.LogWarning($"No {typeof(LocalizedAudioController)} on the scene ");
+                return null;
+            }
+
+            if (LocalizedCaptionsController.Instance == null)
+            {
+                Debug.LogWarning($"No {typeof(LocalizedCaptionsController)} on the scene ");
+                return null;
+            }
+
+
+            if (_PreloadCoroutine != null)
+            {
+                StopCoroutine(_PreloadCoroutine);
+                _PreloadCoroutine = null;
+                Debug.LogWarning("Another preload was on course");
+            }
+
+            _PreloadCoroutine = StartCoroutine(PreloadNarrationCoroutine(speech));
+
+            return _PreloadCoroutine;
         }
 
-
-        if (_PreloadCoroutine != null)
+        private IEnumerator PreloadNarrationCoroutine(Speech speech)
         {
-            StopCoroutine(_PreloadCoroutine);
+            if (speech == null)
+            {
+                Debug.LogWarning($"Null speech.");
+                _PreloadCoroutine = null;
+                yield break;
+            }
+
+            Coroutine audioPreloadCoroutine = LocalizedAudioController.Preload(speech.LocalizedAudio);
+            Coroutine captionsPreloadCoroutine = LocalizedCaptionsController.Preload(speech.LocalizedTextAsset);
+
+            yield return audioPreloadCoroutine;
+            yield return captionsPreloadCoroutine;
+
+
             _PreloadCoroutine = null;
-            Debug.LogWarning("Another preload was on course");
+
+
+            Debug.Log("Preloaded");
         }
 
-        _PreloadCoroutine = StartCoroutine(PreloadNarrationCoroutine(speech));
-
-        return _PreloadCoroutine;
-    }
-
-    private IEnumerator PreloadNarrationCoroutine(Speech speech)
-    {
-        if (speech == null)
+        public static void Play(Speech speech)
         {
-            Debug.LogWarning($"Null speech.");
-            _PreloadCoroutine = null;
-            yield break;
+            if (Instance == null)
+            {
+                Debug.LogWarning($"No {typeof(LocalizedNarrationController)} on the scene ");
+                return;
+            }
+
+            Instance.PlayNarration(speech);
         }
 
-        Coroutine audioPreloadCoroutine = LocalizedAudioController.Preload(speech.LocalizedAudio);
-        Coroutine captionsPreloadCoroutine = LocalizedCaptionsController.Preload(speech.LocalizedTextAsset);
-
-        yield return audioPreloadCoroutine;
-        yield return captionsPreloadCoroutine;
-
-
-        _PreloadCoroutine = null;
-
-
-        Debug.Log("Preloaded");
-    }
-
-    public static void Play(Speech speech)
-    {
-        if (Instance == null)
+        public void PlayNarration(Speech speech)
         {
-            Debug.LogWarning($"No {typeof(LocalizedNarrationController)} on the scene ");
-            return;
+            if (_SpeechCoroutine != null)
+            {
+                StopCoroutine(_SpeechCoroutine);
+                _SpeechCoroutine = null;
+                Debug.LogWarning("Another speech was beeing played");
+            }
+
+            _SpeechCoroutine = StartCoroutine(PlaySpeechCoroutine(speech));
         }
 
-        Instance.PlayNarration(speech);
-    }
-
-    public void PlayNarration(Speech speech)
-    {
-        if (_SpeechCoroutine != null) 
+        private IEnumerator PlaySpeechCoroutine(Speech speech)
         {
-            StopCoroutine(_SpeechCoroutine);
-            _SpeechCoroutine = null;
-            Debug.LogWarning("Another speech was beeing played");
-        }
 
-        _SpeechCoroutine = StartCoroutine(PlaySpeechCoroutine(speech));
-    }
+            yield return PreloadNarration(speech);
 
-    private IEnumerator PlaySpeechCoroutine(Speech speech)
-    {
+            LocalizedAudioController.Play(speech.LocalizedAudio);
+            LocalizedCaptionsController.Play(speech.LocalizedTextAsset);
 
-        yield return PreloadNarration(speech);
-
-        LocalizedAudioController.Play(speech.LocalizedAudio);
-        LocalizedCaptionsController.Play(speech.LocalizedTextAsset);
-
-        _SpeechCoroutine = null;
-    }
-
-    public static void Stop()
-    {
-        if (Instance == null)
-        {
-            Debug.LogWarning($"No {typeof(LocalizedNarrationController)} on the scene ");
-            return;
-        }
-
-        Instance.StopLocalizedNarration();
-    }
-
-    public void StopLocalizedNarration()
-    {
-        if (_SpeechCoroutine != null)
-        {
-            Debug.LogWarning("Stopping the localized narration.");
-
-            StopCoroutine(_SpeechCoroutine);
             _SpeechCoroutine = null;
         }
 
-        LocalizedCaptionsController.Stop();
-        LocalizedAudioController.Stop();
-    }
-
-    #endregion
-
-    #region Init
-    private void CheckSingleton()
-    {
-        // Check if this instance is a duplicated
-        if (_Instance != null && _Instance != this)
+        public static void Stop()
         {
-            Debug.LogWarning($"Multiple instances of <b>{GetType().Name}</b>\nDestroying the component in <b>{name}</b>.");
-            Destroy(this);
-            return;
+            if (Instance == null)
+            {
+                Debug.LogWarning($"No {typeof(LocalizedNarrationController)} on the scene ");
+                return;
+            }
+
+            Instance.StopLocalizedNarration();
         }
 
-        // Set this instance as the selected
-        _Instance = this;
-    }
-    #endregion
+        public void StopLocalizedNarration()
+        {
+            if (_SpeechCoroutine != null)
+            {
+                Debug.LogWarning("Stopping the localized narration.");
 
+                StopCoroutine(_SpeechCoroutine);
+                _SpeechCoroutine = null;
+            }
+
+            LocalizedCaptionsController.Stop();
+            LocalizedAudioController.Stop();
+        }
+
+        #endregion
+
+        #region Init
+        private void CheckSingleton()
+        {
+            // Check if this instance is a duplicated
+            if (_Instance != null && _Instance != this)
+            {
+                Debug.LogWarning($"Multiple instances of <b>{GetType().Name}</b>\nDestroying the component in <b>{name}</b>.");
+                Destroy(this);
+                return;
+            }
+
+            // Set this instance as the selected
+            _Instance = this;
+        }
+        #endregion
+
+    }
 }
--- Runtime/Scriptable Object/Speech.cs
+++ Runtime/Scriptable Object/Speech.cs
@@ -2,9 +2,13 @@ using LocalizedAudio;
 using UnityEngine;
 using UnityEngine.Localization;
 
-[CreateAssetMenu(fileName = "Speech", menuName = "Fenikkel/Speech", order = 2)]
-public class Speech : ScriptableObject
+namespace LocalizedNarration
 {
-    public LocalizedAudioClip LocalizedAudio;
-    public LocalizedTextAsset LocalizedTextAsset; // .srt file (or .txt)
+
+    [CreateAssetMenu(fileName = "Speech", menuName = "Fenikkel/Speech", order = 2)]
+    public class Speech : ScriptableObject
+    {
+        public LocalizedAudioClip LocalizedAudio;
+        public LocalizedTextAsset LocalizedTextAsset; // .srt file (or .txt)
+    }
 }
